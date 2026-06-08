83670b429322c7c07c0996c8d4dc3b9b6c20c1ee
Eder <edergracia006@gmail.com>
2026-06-08 18:10:55 +0200

    .

---
+++ RepasoJ2/src/ra7j1/Mascota.java
@@ -0,0 +1,59 @@
+package ra7j1;
+
+public abstract class Mascota {
+
+	private String nombre;
+	private Integer edad;
+	private Double peso;
+	
+	/**
+	 * @param nombre
+	 * @param edad
+	 * @param peso
+	 */
+	public Mascota(String nombre, Integer edad, Double peso) {
+		this.nombre = nombre;
+		this.edad = edad;
+		this.peso = peso;
+	}
+	
+	/**
+	 * @return the nombre
+	 */
+	public String getNombre() {
+		return nombre;
+	}
+	/**
+	 * @param nombre the nombre to set
+	 */
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	/**
+	 * @return the edad
+	 */
+	public Integer getEdad() {
+		return edad;
+	}
+	/**
+	 * @param edad the edad to set
+	 */
+	public void setEdad(Integer edad) {
+		this.edad = edad;
+	}
+	/**
+	 * @return the peso
+	 */
+	public Double getPeso() {
+		return peso;
+	}
+	/**
+	 * @param peso the peso to set
+	 */
+	public void setPeso(Double peso) {
+		this.peso = peso;
+	}
+	
+	
+	
+}//fin Mascota
